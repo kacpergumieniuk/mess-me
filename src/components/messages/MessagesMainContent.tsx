05e86cc6b9e139bae7ac8cfc2a53dd6df96f547d
Kacper Gumieniuk <kacpergumieniuk@gmail.com>
2023-02-09 23:30:32 +0100

    Change vealue

--- src/components/messages/MessagesMainContent.tsx
+++ src/components/messages/MessagesMainContent.tsx
@@ -6,7 +6,7 @@ export const MessagesMainContent = () => {
     <div className="flex-1 overflow-auto px-[24px] pb-[16px]">
       <div className=" flex flex-col first:mt-[16px]">
         <Message author={"interlocutor"} value={"edsddd"} />
-        <Message author={"user"} value={"siema"} />
+        <Message author={"user"} value={"siemasss"} />
       </div>
     </div>
   );
